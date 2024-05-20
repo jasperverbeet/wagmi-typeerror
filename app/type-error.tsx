4e3cd5ec2395018e610bacb452f4985b8093e7cf
Jasper Verbeet <jasperverbeet@gmail.com>
2024-05-20 13:13:13 +0200

    chore: adds basic example

---
+++ app/type-error.tsx
@@ -0,0 +1,19 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { erc20Abi } from 'viem';
+import { useConfig } from 'wagmi';
+import { readContractQueryOptions } from 'wagmi/query';
+
+
+const useOtcSummary = () => {
+  const config = useConfig();
+
+  const options = readContractQueryOptions(config, {
+    address: '0x1234567890123456789012345678901234567890',
+    abi: erc20Abi,
+    functionName: 'name',
+  });
+
+  return useSuspenseQuery(options);
+};
+
+export default useOtcSummary;
